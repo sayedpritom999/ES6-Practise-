8361bf470181129be9b1871e335ffac10f543332
sayedpritom999 <sayedpritom999@gmail.com>
2020-04-04 21:21:45 +0600

    nth commit

---
+++ map-filters.js
@@ -0,0 +1,69 @@
+// const numbers = [2,3,4,5,6,7,8,9,];
+/* const output = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output); */
+// console.log(numbers);
+
+/* const square = element => element * element;
+const square = x => x * x;
+
+const result = numbers.map(function(element){
+    return element * element;
+})
+ */
+/* const result = numbers.map(x => x*x);
+const bigger = numbers.filter(x => x < 5)
+const find = numbers.find(x => x < 5)
+
+console.log(result, bigger, find); */
+
+// let arr = [1, -2, 3, 4, -4, 5, 6, 20];
+// let arr1 = [1, -2, 3, 4, -4, 5, 6, 80];
+
+
+/* let sum = 0;
+for (let order of arr) 
+sum += order;
+
+ */
+// const sum = arr.reduce((accumulator, currentValue) =>  accumulator + currentValue);
+
+// console.log(sum)
+
+/* const people = [
+    {
+        name: "Tanvir",
+        skill: "Javascript"
+    }, 
+    {
+        name: "Pritom",
+        skill: "Javascript"
+    }, 
+    {
+        name: "Jibon",
+        skill: "Java"
+    }
+]
+
+function isJSdev (skill) {
+    return skill.skill === "Javascript"
+} 
+
+console.log(people.reverse().find(isJSdev).name);
+ */
+
+
+
+
+
+
+
+
+
+
+ 
