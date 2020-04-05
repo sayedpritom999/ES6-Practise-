9958b99dc0767c0fc93bd23908063d3417787367
sayedpritom999 <sayedpritom999@gmail.com>
2020-04-05 17:43:43 +0600

    commit

---
+++ scope.js
@@ -0,0 +1,13 @@
+function sum(first, second) {
+    let result = first + second;
+    if (result > 9) {
+        var mood = "I'm Happy!";
+        //console.log(mood);
+    } console.log(mood); 
+    return result;
+}
+
+sum(2, 8);
+
+console.log(day)
+var day = 'sunday';
